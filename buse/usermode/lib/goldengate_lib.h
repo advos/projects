7ba8ec958cfaff22251bf90990ab66d2865fc84c
Ubuntu 13.04 <arik.evdaev@gmail.com>
2013-08-09 05:13:09 -0700

    goldengate_lib

---
+++ buse/usermode/lib/goldengate_lib.h
@@ -0,0 +1,6 @@
+int goldengate_open(void);
+ssize_t goldengate_read(void *buf, size_t nbyte);
+size_t goldengate_write(const void *buf, size_t count); 
+int goldengate_close(void);
+int goldengate_test(void);
+
